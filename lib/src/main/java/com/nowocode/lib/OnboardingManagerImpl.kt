042068011b0fcfc7d5ea5f328bfcb4526bd7b5d6
Martin Nowosad <mrtnnwsd@gmail.com>
2020-12-21 23:20:19 +0100

    Temporarily disabled Landscape Mode

--- lib/src/main/java/com/nowocode/lib/OnboardingManagerImpl.kt
+++ lib/src/main/java/com/nowocode/lib/OnboardingManagerImpl.kt
@@ -2,13 +2,11 @@ package com.nowocode.lib
 
 import android.app.Activity
 import android.content.Context
-import android.view.View
-import android.view.ViewGroup
+import android.content.res.Configuration.ORIENTATION_LANDSCAPE
+import android.util.Log
 import android.widget.FrameLayout
 import com.nowocode.lib.ui.model.OnboardingAction
 import com.nowocode.lib.ui.OnboardingScaffold
-import com.nowocode.lib.ui.model.VerticalPosition
-import java.lang.ref.WeakReference
 
 internal class OnboardingManagerImpl(context: Context) : OnboardingManager {
     private var onBoardingView: OnboardingScaffold = OnboardingScaffold(context)
@@ -47,6 +45,10 @@ internal class OnboardingManagerImpl(context: Context) : OnboardingManager {
     }
 
     override fun start() {
+        if (activity.resources.configuration.orientation == ORIENTATION_LANDSCAPE) {
+            Log.e(this.javaClass.name, "Landscape mode is currently not supported.")
+            return
+        }
         onBoardingView.initAnimator()
         activity.addContentView(
             onBoardingView,
